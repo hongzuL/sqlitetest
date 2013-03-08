48dc4dc952abf5a752862faef4c2be77859e297e
hongzu li <hongzu@ualberta.ca>
2013-03-07 22:29:22 -0700

    database down

--- gen/com/example/sqlitetest/R.java
+++ gen/com/example/sqlitetest/R.java
@@ -15,7 +15,12 @@ public final class R {
     }
     public static final class id {
         public static final int createDatabase=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int delete=0x7f070005;
+        public static final int insert=0x7f070002;
+        public static final int menu_settings=0x7f070006;
+        public static final int select=0x7f070004;
+        public static final int update=0x7f070003;
+        public static final int updateDatabase=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -26,11 +31,16 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int create_db=0x7f040006;
+        public static final int delete=0x7f04000b;
         public static final int desc=0x7f040004;
         public static final int hello_world=0x7f040001;
         public static final int id=0x7f040003;
         public static final int ingredients=0x7f040005;
+        public static final int insert=0x7f040008;
         public static final int menu_settings=0x7f040002;
+        public static final int select=0x7f04000a;
+        public static final int update=0x7f040009;
+        public static final int update_db=0x7f040007;
     }
     public static final class style {
         /** 
--- src/com/example/sqlitetest/DatabaseHelper.java
+++ src/com/example/sqlitetest/DatabaseHelper.java
@@ -62,5 +62,5 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	         super.onOpen(db);       
 	         // TODO     
 	     }     
-	 }
+}
 
